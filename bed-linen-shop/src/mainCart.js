24748d02647076849128049d782f1b5748d1903f
Heorhii <91286488+GertSer@users.noreply.github.com>
2025-06-23 03:28:51 +0200

    refactor: remove unused mainFooter imports and update product details in homeEN.html

--- bed-linen-shop/src/mainCart.js
+++ bed-linen-shop/src/mainCart.js
@@ -1,7 +1,6 @@
 import { renderCart } from './scripts/render-cart';
 import { setupPaymentForm, countrySelector } from './scripts/payment-handler';
 import '../src/mainCart.scss';
-import './components/mainFooter.js';
 import './components/mainHeader.js';
 
 document.addEventListener('DOMContentLoaded', () => {
--- bed-linen-shop/src/mainShop.js
+++ bed-linen-shop/src/mainShop.js
@@ -1,5 +1,4 @@
 import { renderAllProduct } from './scripts/product.js';
 import '../src/mainProducts.scss';
-import './components/mainFooter.js';
 import './components/mainHeader.js';
 renderAllProduct();
